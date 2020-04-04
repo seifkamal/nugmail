211ec9311a2c8d86f8cfebb23283da3327e0c988
Seif Kamal <seif@tickx.co.uk>
2020-04-04 15:51:18 +0100

    Build address list command

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ type StdError = Box<dyn std::error::Error>;
 fn main() {
     let matches = App::new("nug")
         .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(SubCommand::with_name("list"))
         .subcommand(SubCommand::with_name("new"))
         .subcommand(
             SubCommand::with_name("inbox")
@@ -28,6 +29,15 @@ fn main() {
         .get_matches();
 
     match matches.subcommand() {
+        ("list", _) => {
+            let storage_connection = sqlite::default_connection().unwrap();
+            let mut storage = sqlite::EmailStorage::new(&storage_connection).unwrap();
+            let addresses = storage.addresses().unwrap();
+
+            for address in addresses.iter() {
+                println!("{}", address)
+            }
+        }
         ("new", _) => {
             let client = generator::webhook_site::Client::new();
             let address = client.generate().unwrap();
